90a322ef4728b3e8cdb76f2f2e98a191d47e1233
Saurav Dhungana <saurav.dh@gmail.com>
2018-11-01 01:27:17 +0545

    shortest path function completed

--- src/samudaya/algo/centrality.clj
+++ src/samudaya/algo/centrality.clj
@@ -1,38 +1,65 @@
 (ns samudaya.algo.centrality
   (:require [loom.graph :refer (nodes edges successors)]
-            [loom.alg-generic :as gen]
             [loom.alg :as alg]))
 
 ;; Based on Newman-Girvan (2004) Shortest-path edge betweenness method
 ;; This is based on the "Algorithm 1" and "Algorithms 7" from Brandes (2008)
 
+(defn initialize-state
+  "Represents the initial algorithmic state as an immutable map"
+  [g start]
+  (let [nodeset (disj (nodes g) start)]
+    {:graph g
+     :nodeset nodeset
+     :predecessors (apply assoc {} (interleave (nodes g) (repeat [])))
+     :distance (apply assoc {start 0} (interleave nodeset (repeat -1)))
+     :path-count (apply assoc {start 1} (interleave nodeset (repeat 0)))
+     :stack []
+     :queue (conj clojure.lang.PersistentQueue/EMPTY start)
+     :current-node nil}))
+
+(defn next-node [state]
+  (let [v (peek (:queue state))]
+    (-> state
+        (update :stack conj v)
+        (assoc :current-node v))))
+
+(defn process-successor [state w]
+  (let [current-node (:current-node state)
+        dist-w ((:distance state) w)
+        dist-v ((:distance state) current-node)
+        sigma-w ((:path-count state) w)
+        sigma-v ((:path-count state) current-node)]
+    (cond
+      (= dist-w -1) (-> state
+                        (update :queue conj w)
+                        (update-in [:distance] assoc w (inc dist-v))
+                        (update-in [:path-count] assoc w (+ sigma-w sigma-v))
+                        (update-in [:predecessors] update w conj current-node))
+      (= dist-w (inc dist-v)) (-> state
+                                  (update-in [:path-count] assoc w (+ sigma-w sigma-v))
+                                  (update-in [:predecessors] update w conj current-node))
+      :else state)))
+
+(defn process-successors [state]
+  (reduce
+   process-successor
+   state
+   (successors (:graph state) (:current-node state))))
+
+(defn pop-queue [state]
+  (update state :queue pop))
+
+(def next-state (comp pop-queue process-successors next-node))
 
 (defn ss-shortest-path
   "Returns a list of maps with metrics like predecessors, no. of shortest paths,
   list of nodes by distance"
   [g start]
-  (let [nodeset (disj (nodes g) start)
-        pred (apply assoc {} (interleave (nodes g) (repeat nil)))
-        dist (apply assoc {start 0} (interleave nodeset (repeat -1)))
-        sigma (apply assoc {start 1} (interleave nodeset (repeat 0)))
-        stack []]
-    (loop [queue (conj clojure.lang.PersistentQueue/EMPTY start)]
-      (if (empty? queue)
-        {:sigma sigma
-         :pred pred
-         :stack stack}
-        (let [v (peek queue)
-              stack (conj stack v)]
-          (doseq [w (successors g v)]
-            (when (= (dist w) -1)
-              (do
-                (conj queue w)
-                (assoc dist w (+ 1 (dist v)))))
-            (when (= (dist w) (+ 1 (dist v)))
-                  (do
-                    (assoc sigma w (+ (sigma w) (sigma v)))
-                    (assoc pred w v))))
-            (recur (pop queue)))))))
+  (loop [state (initialize-state g start)]
+    (if (empty? (:queue state))
+      state
+      (recur (next-state state)))))
 
 (defn- accumulate
   [state]
