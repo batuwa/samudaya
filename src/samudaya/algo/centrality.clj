a20ac4821c79870caa061b7dd3c40d00310aadc2
Saurav Dhungana <saurav.dh@gmail.com>
2018-11-04 01:23:57 +0545

    edge betweenness completed

--- src/samudaya/algo/centrality.clj
+++ src/samudaya/algo/centrality.clj
@@ -112,24 +112,90 @@
       cbet
       (recur (next-acc-state state) (update-betweenness cbet state)))))
 
-
 ;; Rescale betweenness scores
-(defn finalize-betweenness
-  "Normalize betweenness as it scales with the number of pairs of nodes"
-  [directed betweenness]
-  (let [n (count betweenness)]
-    (cond
-      directed (apply merge (map (fn [[k v]] {k (* v (/ 1 (* (- n 1) (- n 2))))}) betweenness))
-      :else    (apply merge (map (fn [[k v]] {k (* v (/ 2 (* (- n 1) (- n 2))))}) betweenness)))))
-
+(defn- normalize-betweenness
+  "Normalize betweenness based on Wikipedia definition
+   https://en.wikipedia.org/wiki/Betweenness_centrality"
+  [n directed betweenness]
+  (cond
+    directed (apply merge (map (fn [[k v]] {k (* v (/ 1 (* (- n 1) (- n 2))))}) betweenness))
+    :else    (apply merge (map (fn [[k v]] {k (* v (/ 2 (* (- n 1) (- n 2))))}) betweenness))))
 
 ;; Main function
 (defn betweenness-centrality
   [g]
   (let [nodeset (nodes g)
         directed (directed? g)
+        n (count nodeset)
         betweenness (apply assoc {} (interleave nodeset (repeat 0.0)))]
     (->> nodeset
          (pmap #(ss-shortest-path g %))
          (reduce accumulate betweenness)
-         (finalize-betweenness directed))))
+         (normalize-betweenness n directed))))
+
+
+;; Edge betweenness functions
+(defn- update-edge-betweenness [cbet state]
+  (let [current-node (:current-node state)
+        cbet-w (cbet current-node)
+        delta-w ((:delta state) current-node)]
+    (if (not= (:current-node state) (:start state))
+      (-> cbet
+          (assoc current-node (+ cbet-w delta-w)))
+      cbet)))
+
+(defn- legit-edge [state current-node v]
+  (if (contains? (:edge-betweenness state) [v current-node])
+    [v current-node]
+    [current-node v]))
+
+(defn- calculate-edge-dependency [state v]
+  (let [current-node (:current-node state)
+        delta-w ((:delta state) current-node)
+        delta-v ((:delta state) v)
+        sigma-w ((:path-count state) current-node)
+        sigma-v ((:path-count state) v)
+        edge-key (legit-edge state current-node v)
+        coeff (/ (inc delta-w) sigma-w)
+        c (* coeff sigma-v)]
+    (-> state
+        (update-in [:delta] assoc v (+ delta-v c))
+        (update-in [:edge-betweenness] assoc edge-key c)))) ;(+ ((:edge-betweenness state) edge-key) c)))))
+
+(defn- calculate-edge-dependencies [state]
+    (reduce
+     calculate-edge-dependency
+     state
+     ((:predecessors state) (:current-node state))))
+
+(def next-acc-edges-state (comp pop-stack calculate-edge-dependencies next-acc-node))
+
+(defn accumulate-edges
+  [betweenness state]
+  (loop [state (merge state {:delta (apply assoc {} (interleave (nodes (:graph state)) (repeat 0)))
+                             :edge-betweenness (apply assoc {} (interleave (alg/distinct-edges (:graph state)) (repeat 0.0)))})
+         cbet betweenness]
+    (if (empty? (:stack state))
+      (merge-with + cbet (:edge-betweenness state))
+      (recur (next-acc-edges-state state) (update-edge-betweenness cbet state)))))
+
+;; Rescale betweenness scores
+(defn- normalize-edge-betweenness
+  "Normalize betweenness based on Wikipedia definition
+   https://en.wikipedia.org/wiki/Betweenness_centrality"
+  [n directed betweenness]
+  (cond
+    directed (apply merge (map (fn [[k v]] {k (* v (/ 1 (* n (- n 1))))}) betweenness))
+    :else    (apply merge (map (fn [[k v]] {k (* v (/ 2 (* n (- n 1))))}) betweenness))))
+
+(defn edge-betweenness-centrality
+  [g]
+  (let [nodeset (nodes g)
+        directed (directed? g)
+        n (count nodeset)
+        betweenness (apply assoc {} (interleave nodeset (repeat 0.0)))]
+    (apply dissoc (->> nodeset
+                       (pmap #(ss-shortest-path g %))
+                       (reduce accumulate-edges betweenness)
+                       (normalize-edge-betweenness n directed))
+    nodeset)))
